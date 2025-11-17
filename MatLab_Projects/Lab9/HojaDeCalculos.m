bb8d210e016bbab8c08a3093a1853d8d124b6024
simplementeUnPorito <eliasdavidalvarez@gmail.com>
2025-11-16 22:15:35 -0300

    kkk

--- MatLab_Projects/Lab9/HojaDeCalculos.m
+++ MatLab_Projects/Lab9/HojaDeCalculos.m
@@ -48,14 +48,14 @@ P  = K;
 
 
 
-off      = 2.024;   % offset DC de tu planta
 
 
 Vpp_adc = 4.8;    % ±2.4 V
 Nbits_adc = 12;
 
 
-Vpp_dac = 4.024;
+Vpp_dac = 4.048;
+off      = 2.024;   % offset DC de tu planta
 Nbits_dac = 8;
 
 [lsb_adc, sigmaq_adc, snr_adc] = quanti_noise_from_bits(Nbits_adc, Vpp_adc);
@@ -66,21 +66,26 @@ Nbits_dac = 8;
 f_pts  = [0.01 0.1 2 10 100 1000]*1e3;      % Hz, ejemplo
 S_nV   = [700 300 100 60 50 45];     % nV/sqrt(Hz), ejemplo
 S_pts  = S_nV * 1e-9;                       % V/sqrt(Hz)
-
-poles_plant = [-1/(C1*R2), -1/(C2*R4)];           % rad/s
-
-[vrms_unf, vrms_filt] = noise_from_psd_with_poles(f_pts, S_pts, poles_plant);
-
 BW = 500;
 G1 = R2/R1;G2=R4/R3;
+
+poles_plant = [-1/(C1*R2)];           % rad/s
+
+[vrms_unf, vrms_filt] = noise_from_psd_with_poles(f_pts, S_pts, poles_plant);
+vrms_filt1 = G1*vrms_filt;
+
+poles_plant = [-1/(C2*R4)];
+[vrms_unf, vrms_filt] = noise_from_psd_with_poles(f_pts, S_pts, poles_plant);
+vrms_filt2 = G2*vrms_filt;
+
 
 [vrms_R1, en_R1] = resistor_noise(R1, BW, G1);
 [vrms_R2, en_R2] = resistor_noise(R2, BW, 1);
 [vrms_R3, en_R3] = resistor_noise(R3, BW, G2);
 [vrms_R4, en_R4] = resistor_noise(R4, BW, 1);
 
-sigma_x1 = sqrt(vrms_unf^2 + vrms_R1^2 + vrms_R2^2 + sigmaq_dac^2);
-sigma_x2 = sqrt(vrms_unf^2 + vrms_R3^2 + vrms_R4^2);
+sigma_x1 = sqrt(vrms_filt1^2 + vrms_R1^2 + vrms_R2^2 + sigmaq_dac^2);
+sigma_x2 = sqrt(vrms_filt2^2 + vrms_R3^2 + vrms_R4^2);
 
 
 
