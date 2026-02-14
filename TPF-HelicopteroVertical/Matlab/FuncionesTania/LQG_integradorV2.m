971693bfed0b6bc89540382fcaf73df26094737a
Voghy <70350252+Voghy@users.noreply.github.com>
2026-02-14 19:42:10 -0300

    XDDDDDDDDDDDDD
    XD

--- TPF-HelicopteroVertical/Matlab/FuncionesTania/LQG_integradorV2.m
+++ TPF-HelicopteroVertical/Matlab/FuncionesTania/LQG_integradorV2.m
@@ -184,8 +184,13 @@ D = 0;
 sysDaug = ss(Aaug,Baug,Caug,D,Ts);
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 sysObs = ss((A-Lk*C*A),B,C,D,Ts);
 figure;pzmap(sysDaug);grid on;zgrid;hold on;pzmap(sysObs);
+=======
+
+figure;pzmap(sysDaug);grid on;zgrid;hold on;pzmap()
+>>>>>>> Stashed changes
 =======
 
 figure;pzmap(sysDaug);grid on;zgrid;hold on;pzmap()
