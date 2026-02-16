7bcccf9971e0838334f9801e48d48943a0d51a6a
simplementeUnPorito <eliasdavidalvarez@gmail.com>
2026-02-16 14:47:11 -0300

    486554

--- TPF-HelicopteroVertical/Matlab/FuncionesTania/porsi.m
+++ TPF-HelicopteroVertical/Matlab/FuncionesTania/porsi.m
@@ -1,30 +1,77 @@
-close all
-clear all
-clc
-load("D:\auto\TPF-HelicopteroVertical\Matlab\modeloSimplificado (1).mat");
+close all; clear; clc
 
+%% ===== 1) Cargar planta continua desde .mat =====
+S = load('D:\auto\TPF-HelicopteroVertical\Matlab\planta (1).mat');
 
+if isfield(S,'plantaC')
+    plantaC = S.plantaC;      % <- tu variable típica
+elseif isfield(S,'sysC')
+    plantaC = S.sysC;
+else
+    error('No encuentro "plantaC" ni "sysC" en el .mat');
+end
+
+%% ===== 2) A,B,C,D CONTINUO =====
+sysC = ss(plantaC);           % asegura forma espacio de estados
+[Ac,Bc,Cc,Dc] = ssdata(sysC);
+
+disp('=== CONTINUO ===');
+disp('Ac ='); disp(Ac);
+disp('Bc ='); disp(Bc);
+disp('Cc ='); disp(Cc);
+disp('Dc ='); disp(Dc);
+
+%% ===== 2.1) Controlabilidad/Observabilidad (CONTINUO) =====
+n = size(Ac,1);
+
+CtrbC = ctrb(Ac,Bc);
+ObsvC = obsv(Ac,Cc);
+
+% ranks (con y sin tolerancia explícita)
+tolC = 1e-9;   % podés ajustar si querés
+rankCtrbC = rank(CtrbC);
+rankObsvC = rank(ObsvC);
+rankCtrbC_tol = rank(CtrbC, tolC);
+rankObsvC_tol = rank(ObsvC, tolC);
+
+disp('--- CONTINUO: Controlabilidad / Observabilidad ---');
+disp('CtrbC = ctrb(Ac,Bc) ='); disp(CtrbC);
+disp('ObsvC = obsv(Ac,Cc) ='); disp(ObsvC);
+fprintf('rank(CtrbC) = %d (de %d) | con tol=%.1e -> %d\n', rankCtrbC, n, tolC, rankCtrbC_tol);
+fprintf('rank(ObsvC) = %d (de %d) | con tol=%.1e -> %d\n', rankObsvC, n, tolC, rankObsvC_tol);
+
+%% ===== 3) A,B,C,D DISCRETO (Ts = 1 ms) =====
 Ts = 1/1000;
-sysD = c2d(sysC,Ts,'zoh');
-%% PID
-% Kp = 2.5;Ti = 5;Td = 0.1; N = 3;
-% numCA = Kp*[Ti*Td, (Ti + Td/N), 1]; % Ojo con la forma exacta de tu numCA
-% denCA = [Ti*Td/N, Ti, 0];
-% controlador_Ams = tf(numCA, denCA);
-% C = c2d(controlador_Ams,Ts,'tustin');
+sysD = c2d(sysC, Ts, 'zoh');  % ZOH recomendado para planta con hold
 
-%% Lugar de raices
+[Ad,Bd,Cd,Dd] = ssdata(sysD);
 
-C = tf(-0.017346322429932.*conv([1,-1.014028439216919],[1,-0.5]),conv([1,-0.952242280789835],[1,-0.989400000000000]),Ts);
-%% Graficos
-C
-zpk(C)
-figure;bode(C*sysD); grid on;grid minor;
-figure;rlocus(C*sysD); zgrid;
-figure;step(feedback(C*sysD,1));grid on; grid minor;
-figure;step(feedback(C,sysD));grid on;grid minor;
+disp('=== DISCRETO (ZOH) ===');
+fprintf('Ts = %.9f s\n', Ts);
+disp('Ad ='); disp(Ad);
+disp('Bd ='); disp(Bd);
+disp('Cd ='); disp(Cd);
+disp('Dd ='); disp(Dd);
 
+%% ===== 3.1) Controlabilidad/Observabilidad (DISCRETO) =====
+CtrbD = ctrb(Ad,Bd);
+ObsvD = obsv(Ad,Cd);
 
+tolD = 1e-9;
+rankCtrbD = rank(CtrbD);
+rankObsvD = rank(ObsvD);
+rankCtrbD_tol = rank(CtrbD, tolD);
+rankObsvD_tol = rank(ObsvD, tolD);
 
+disp('--- DISCRETO: Controlabilidad / Observabilidad ---');
+disp('CtrbD = ctrb(Ad,Bd) ='); disp(CtrbD);
+disp('ObsvD = obsv(Ad,Cd) ='); disp(ObsvD);
+fprintf('rank(CtrbD) = %d (de %d) | con tol=%.1e -> %d\n', rankCtrbD, n, tolD, rankCtrbD_tol);
+fprintf('rank(ObsvD) = %d (de %d) | con tol=%.1e -> %d\n', rankObsvD, n, tolD, rankObsvD_tol);
 
-
+%% ===== 4) (Opcional) Exportar a .mat para usar después =====
+save('ABCD_cont_y_disc.mat', ...
+     'Ac','Bc','Cc','Dc','Ad','Bd','Cd','Dd','Ts', ...
+     'CtrbC','ObsvC','rankCtrbC','rankObsvC','rankCtrbC_tol','rankObsvC_tol', ...
+     'CtrbD','ObsvD','rankCtrbD','rankObsvD','rankCtrbD_tol','rankObsvD_tol', ...
+     'tolC','tolD');
